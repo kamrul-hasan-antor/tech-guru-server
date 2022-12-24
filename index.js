e9901dd7306c1c603c55e4ad56175d2a071445fe
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2022-12-24 12:22:12 +0600

    categories id added

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ app.use(cors());
 
 const categories = require("./data/categories.json");
 
+const courses = require("./data/courses.json");
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCategory = courses.find((course) => course.category_id === id);
+  res.send(selectedCategory);
 });
 
 app.listen(port, () => {
