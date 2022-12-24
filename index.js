5a3117cac8e677d6ef9fa7c3f56eda878e73c3b7
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2022-12-24 11:54:21 +0600

    categories added

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = 5000;
 
 app.use(cors());
 
+const categories = require("./data/categories.json");
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
